e1d464c00a4da6ddec379a4c56366578960ae88d
yewonkim301 <yeowne301@gmail.com>
2024-11-05 12:19:32 +0900

    폴더별 관심상품 조회 구현 - query 주석 추가

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -13,4 +13,23 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId,
         Pageable pageable);
+    /* select * from
+        product p left join product_folder pf
+            on p.id = pf.product_id
+        where p.user_id = 1
+            and
+            pf.folder_id = 3;
+    */
+
+    /* 페이징 기능까지 하려면
+        select * from
+        product p left join product_folder pf
+            on p.id = pf.product_id
+        where p.user_id = 1
+            and
+            pf.folder_id = 3
+        order by p.id desc
+            limit 10; // 앞에 숫자 생략하고 앞에서 부터 10개만 조회 가능
+            limit 10, 10; // 이렇게 쓸 경우 10(시작하는 숫자)부터 10개 조회
+    */
 }
